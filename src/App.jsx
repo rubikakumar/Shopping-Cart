bd543db58f0960e00911cace58f8e6316eb3c874
Rubika <you@example.com>
2024-09-18 12:02:11 +0530

    Image updated

--- src/App.jsx
+++ src/App.jsx
@@ -52,7 +52,7 @@ const App = () => {
       {}
       <div className="shopping-title-container">
         <img
-          src="https://files.oaiusercontent.com/file-8Nzgytu3haqOc6ir90MYiaYN?se=2024-09-15T14%3A32%3A25Z&sp=r&sv=2024-08-04&sr=b&rscc=max-age%3D604800%2C%20immutable%2C%20private&rscd=attachment%3B%20filename%3D08f5f624-0016-4b48-88ad-a941b3dab4a4.webp&sig=AF704OCX2t1mB2U2wUPSO7XAm6LMiNPlMKWa6bgmHHE%3D"
+          src="https://th.bing.com/th/id/OIP.22AfZHOgi3B_OBQULDEX_AHaCk?w=298&h=110&c=7&r=0&o=5&dpr=1.3&pid=1.7"
           alt="Timeless Treasures for You"
           className="shopping-title-image"
         />
